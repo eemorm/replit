ac3bcdb1399f6b6997e6609bf167a0424677724a
GameBurrito <60300469+eemorm@users.noreply.github.com>
2025-09-01 13:08:27 -0500

    Create main.py

---
+++ light_in_the_dark.py/main.py
@@ -0,0 +1,20 @@
+import os
+import time
+
+print ("Do not exit while icon is on the screen.")
+
+time.sleep(3)
+
+for i in range(21):
+  print ("\ ")
+  time.sleep(0.1)
+  os.system('clear')
+  print ("| ")
+  time.sleep(0.1)
+  os.system('clear')
+  print ("/ ")
+  time.sleep(0.1)
+  os.system('clear')
+  print ("- ")
+  time.sleep(0.1)
+  os.system('clear')
