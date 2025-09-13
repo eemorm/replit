231340cf978fae0fb6490251798b0f1f9dfc47df
GameBurrito <60300469+eemorm@users.noreply.github.com>
2025-09-13 14:51:57 -0700

    Create main.py

---
+++ game.py/main.py
@@ -0,0 +1,59 @@
+def thing():
+  import time
+  import os
+
+  startchoice = input ("""                    Game by   Everett Morman
+
+                      """)
+
+  os.system('clear')
+  print ("Loading.")
+  time.sleep(.5)
+  os.system('clear')
+  print ("Loading..")
+  time.sleep(.5)
+  os.system('clear')
+  print ("Loading...")
+  time.sleep(.5)
+  os.system('clear')
+  print ("Loading.")
+  time.sleep(.5)
+  os.system('clear')
+  print ("Loading..")
+  time.sleep(.5)
+  os.system('clear')
+  print ("Loading...")
+  time.sleep(.5)
+  os.system('clear')
+  print ("Loading.")
+  time.sleep(.5)
+  os.system('clear')
+  print ("Loading..")
+  time.sleep(.5)
+  os.system('clear')
+  print ("Loading...")
+  time.sleep(.5)
+  os.system('clear')
+
+
+
+
+
+
+
+
+
+
+
+
+
+  else:
+    while True:
+      os.system('clear')
+      startchoice = input ("""                    Game by Everett Morman
+
+                        Press P to Start""")
+
+
+
+thing()
