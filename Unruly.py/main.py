abcc8291836ec942501dbcd9ee044964194ce3d6
GameBurrito <60300469+eemorm@users.noreply.github.com>
2025-09-13 14:50:25 -0700

    Create main.py

---
+++ Unruly.py/main.py
@@ -0,0 +1,317 @@
+import time
+import random
+import os
+
+print ("Unruly\n")
+time.sleep(3)
+os.system('clear')
+
+task = " Task: Protect the Persian Sword\n\n"
+instructions = "Instructions: Sneak into Fluffl's Castle and put a WHB on it for anyone who trys to steal it.\n"
+print (task, instructions)
+time.sleep(5)
+os.system('clear')
+time.sleep(1)
+
+print ("""
+          Inventory: WNB, Fighter's Sword
+
+              ^
+             ^ ^
+            |   |
+           |     |
+           -------
+
+
+                  <>""")
+
+time.sleep(.5)
+os.system('clear')
+
+print ("""
+          Inventory: WNB, Fighter's Sword
+
+              ^
+             ^ ^
+            |   |
+           |     |
+           -------
+
+
+                <>""")
+
+time.sleep(.5)
+os.system('clear')                
+
+print ("""
+          Inventory: WNB, Fighter's Sword
+
+              ^
+             ^ ^
+            |   |
+           |     |
+           -------
+
+
+              <>""")
+
+time.sleep(.5)
+os.system('clear')
+
+print ("""
+          Inventory: WNB, Fighter's Sword
+
+              ^
+             ^ ^
+            |   |
+           |     |
+           -------
+
+              <>
+                    """)
+
+time.sleep(.5)
+os.system('clear')
+
+print ("""
+          Inventory: WNB, Fighter's Sword
+
+              ^
+             ^ ^
+            |   |
+           |     |
+           -------
+              <>
+              
+                    """)
+
+time.sleep(.5)
+os.system('clear')
+
+print ("""
+          Inventory: WNB, Fighter's Sword
+
+              ^
+             ^ ^
+            |   |
+           |     |
+           -------
+              
+              
+                    """)
+
+time.sleep(.5)
+os.system('clear')
+time.sleep(2)
+
+print ("""
+          Inventory: WNB, Fighter's Sword
+
+
+            ______________________
+            |                     |
+            ----------------------  
+            |                     |
+            ----------------------
+
+                      <>""")
+
+time.sleep(.5)
+os.system('clear')
+
+print ("""
+          Inventory: WNB, Fighter's Sword
+
+
+            ______________________
+            |                     |
+            ----------------------  
+            |         <>          |
+            ----------------------
+
+                        """)
+
+time.sleep(.5)
+os.system('clear')
+
+print ("""
+          Inventory: WNB, Fighter's Sword
+
+
+            ______________________
+            |         <>          |
+            ----------------------  
+            |                     |
+            ----------------------
+
+                        """)
+
+time.sleep(.5)
+os.system('clear')
+
+print ("""
+          Inventory: WNB, Fighter's Sword
+
+                      <>
+            ______________________
+            |                     |
+            ----------------------  
+            |                     |
+            ----------------------
+
+                      """)
+
+time.sleep(.5)
+os.system('clear')
+time.sleep(2)
+
+print ("""
+          Inventory: WNB, Fighter's Sword
+
+
+          ___________
+          |         |
+          |         |
+          |   ----  |      <>
+          |         |
+          -----------     _--_
+
+
+          PRESS ANY BUTTON TO PLACE""")
+
+answnbreally = input ("")
+
+os.system('clear')
+time.sleep(1)
+
+print ("""
+          Inventory: WNB, Fighter's Sword
+
+
+          ___________
+          |         |
+          |   =+=   |
+          |   ----  |      <>
+          |         |
+          -----------     _--_
+
+
+          You placed it! You also connect it to the app on your phone so you can see what happens to the WNB.""")
+
+time.sleep(5)
+os.system('clear')
+time.sleep(2)
+
+print ("THE NEXT MORNING...")
+time.sleep(2)
+os.system('clear')
+
+print ("""
+          ---------
+          |       |
+          |       |
+          |       |    BEEP!
+          |       |
+          ---------""")
+
+time.sleep(2)
+os.system('clear')
+time.sleep(1)
+
+print ("""
+                ---------
+                |       |
+                |       |
+                |       |
+                |       |
+                ---------
+
+You answer the call; it's King Fluffl! 'The Persian Sword has been stolen! We need your help, Player!' They also said they think the thief is on the other side of the world.""")
+
+time.sleep(10)
+os.system('clear')
+time.sleep(2)
+
+print ("You immediately pack up stuff for the trip. You know where to go; the village of Dilormia. You finish packing up and go out the door.")
+
+time.sleep(10)
+os.system('clear')
+
+print ("9 MONTHS AND 27 DAYS LATER...")
+
+time.sleep(2)
+os.system('clear')
+
+print ("YOU MADE IT")
+
+time.sleep(2)
+os.system('clear')
+time.sleep(2)
+
+print ("You ask a villager where Ellie Mae Pups is. They say in that big building (THAT was easy to find). So you go over there and enter.")
+
+time.sleep(5)
+os.system('clear')
+
+print ("CHAPTER 2")
+time.sleep(2)
+os.system('clear')
+
+secrets1 = "Kitti in a Box!"
+
+secret = input ("Any Secrets? ")
+if secret in secrets1:
+  os.system('clear')
+  print ("Dat my Fluffy Boy!")
+  time.sleep(3)
+  os.system('clear')
+else:
+  os.system('clear')
+  print ("Onward!")
+  time.sleep(2)
+  os.system('clear')
+
+talk1 = input ("'I see you have found us.'")
+
+os.system('clear')
+
+talk2 = input ("'I did. How did you take the sword, though?'")
+
+os.system('clear')
+
+talk3 = input ("'My assistant Faith disarmed the WNB and took the sword. I bet you can figure out why.'")
+
+os.system('clear')
+
+whyans = "King Fluffl is bad"
+
+why = input ("Why? ")
+if why in whyans:
+  os.system('clear')
+  print ("Ellie Mae Pups nods at you and says 'Yes.'")
+  time.sleep(4)
+  os.system('clear')
+else:
+  os.system('clear')
+  print ("She says in a serious tone: 'King Fluffl is bad'")
+  time.sleep(4)
+  os.system('clear')
+
+talk4 = input ("Thousands of thoughts blaze through your head but only one sticks out: I have to kill him.")
+
+os.system('clear')
+
+talk5 = input ("'Faith took the sword because he would have used it to destroy the world.'")
+
+os.system('clear')
+
+talk6 = input ("'Thank you. I will take him down.'")
+
+os.system('clear')
+
+talk7 = input ("'I always knew you would!'")
+
+os.system('clear')
+
+print ("CHAPTER 3")
+time.sleep(2)
